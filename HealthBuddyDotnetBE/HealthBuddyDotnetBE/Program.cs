6671207296dd0ee5a4ae7cd565e9176a5b976029
Nidhi <nidhinayak31@gmail.com>
2025-01-03 16:21:13 +0530

    commit -m "basic repo & context template"

--- HealthBuddyDotnetBE/HealthBuddyDotnetBE/Program.cs
+++ HealthBuddyDotnetBE/HealthBuddyDotnetBE/Program.cs
@@ -1,8 +1,29 @@
+using HealthBuddyDotnetBE.Contexts;
+using HealthBuddyDotnetBE.Repositories.Declaration;
+using HealthBuddyDotnetBE.Repositories.Implementation;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "HealthBuddyDotnetBE", Version = "v1" });
+});
+
+builder.Services.AddDbContext<HealhBuddyContext>(options =>
+{
+    options.UseMySql(builder.Configuration.GetConnectionString("MyDBString"), new MySqlServerVersion(new Version(8, 0, 11)));
+});
+builder.Services.AddTransient<IDoctorRepository, DoctorRepository>();
+builder.Services.AddTransient<IPatientRepository, PatientRepository>();
+builder.Services.AddTransient<IAppointmentRepository, AppointmentRepository>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IAppointmentRepository, AppointmentRepository>();
+builder.Services.AddTransient<ITimeSlotRepository, TimeSlotRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- HealthBuddyDotnetBE/HealthBuddyDotnetBE/obj/Debug/net8.0/HealthBuddyDotnetBE.AssemblyInfo.cs
+++ HealthBuddyDotnetBE/HealthBuddyDotnetBE/obj/Debug/net8.0/HealthBuddyDotnetBE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthBuddyDotnetBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fea29162b0807a94dae432ee44400f6b5bda3baa")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthBuddyDotnetBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthBuddyDotnetBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
